6c3d5a3d20a4449d808331c07329002cb7fe36c0
naveen kala <kalanaveen55@gmail.com>
2022-11-04 14:31:20 +0530

    header added

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
-
-import './Header.css';
+import React from "react";
+import SubHeading from "../../components/SubHeading/SubHeading";
+import { images } from "../../constants";
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="chase the new flavour" />
+      <h1 className="app__header-h1">The Key To Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa iusto
+        placeat, sapiente dicta autem nostrum. Odit quam ex, blanditiis sit
+        ducimus voluptatibus illo consequuntur repellendus, eius maiores eum
+        perspiciatis optio.
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header_img" />
+    </div>
   </div>
 );
 
